693df19efa75ef793efa3144f258bc088c93b762
Kevin Bertrand <kevinbertrand68.pro@gmail.com>
2026-02-27 16:56:55 +0100

    feat: take_dongle and release dongle logic added

--- coders/sim.c
+++ coders/sim.c
@@ -6,22 +6,49 @@
 /*   By: kebertra <kebertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 17:13:52 by kebertra          #+#    #+#             */
-/*   Updated: 2026/02/26 17:49:12 by kebertra         ###   ########.fr       */
+/*   Updated: 2026/02/27 16:53:47 by kebertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "coders.h"
+
+void	take_dongle(t_coder *coder, t_dongle *dongle)
+{
+	pthread_mutex_lock(&dongle->lock);
+	log_message(coder, "has taken a dongle");
+}
+
+void	release_dongle(t_coder *coder, t_dongle *dongle)
+{
+	pthread_mutex_unlock(&dongle->lock);
+	dongle->time_end_cooldown = get_timestamp() + coder->sim->time_cooldown;
+}
 
 void	*coder_routine(void *arg)
 {
 	t_coder	*self;
 
 	self = (t_coder *)arg;
+	self->time_last_compile = get_timestamp();
 	while (self->nb_compile < self->sim->total_compile)
 	{
-		log_message(self, "coucou");
+		take_dongle(self, self->right_dongle);
+		take_dongle(self, self->left_dongle);
+
+		log_message(self, "is compiling");
+		mysleep(self->sim->time_compile);
+
+		release_dongle(self, self->right_dongle);
+		release_dongle(self, self->left_dongle);
+
+		self->time_last_compile = get_timestamp();
+
+		log_message(self, "is debugging");
+		mysleep(self->sim->time_debug);
+
+		log_message(self, "is refactoring");
+		mysleep(self->sim->time_refactor);
 		self->nb_compile++;
-		mysleep(5000);
 	}
 	return (NULL);
 }
